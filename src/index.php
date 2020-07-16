f145d46a234c52b95e2e5af4dc7a7d9cbd7a3bf2
csedo <hello@kelczdavid.hu>
2020-07-16 14:04:50 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
     require_once('Controller/ClassAutoLoader.php');
     try {
         new ClassAutoLoader();
+        SessionController::InitSession();
         SiteGenerator::RenderSite();
     } catch (\Throwable $th) {
         new ExceptionHandler($th);
